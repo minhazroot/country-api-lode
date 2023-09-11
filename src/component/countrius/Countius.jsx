d760e60ae805f9b067758ea525a6a6997bb30a05
minhazroot <minhazuddin180016@gmail.com>
2023-09-11 19:26:53 +0600

    add list

--- src/component/countrius/Countius.jsx
+++ src/component/countrius/Countius.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
 import Country from "../country/Country";
+import './Countius.css'
 
 const Countius = () => {
     const [Countrie, setCountrie] = useState([])
+    const [visitedcountry, setvisitedcountry] = useState([])
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
@@ -10,14 +13,34 @@ const Countius = () => {
 
 
     }, [])
+    const handelvisitedcountry = COUNTRY => {
+        console.log('add to your vesited country ')
+        console.log(COUNTRY)
+        const newvisitedcountry = [...visitedcountry, COUNTRY];
+        setvisitedcountry(newvisitedcountry);
+
+    }
     return (
         <div>
+            <h3>visited Country{visitedcountry.length} </h3>
+            <div>
+                <ul>
+                    {
+                        visitedcountry.map(VCountry => <li key={VCountry.ccn3} >{VCountry?.name?.common}</li>)
+                    }
+
+                </ul>
+            </div>
             <h3>Countries Total: {Countrie.length}</h3>
-            {
-                Countrie.map(country => <Country country={country}
-                    key={country?.ccn3}
-                ></Country>)
-            }
+            <div className="Countius_countener ">
+                {
+                    Countrie.map(country => <Country
+                        country={country}
+                        handelvisitedcountry={handelvisitedcountry}
+                        key={country?.ccn3}
+                    ></Country>)
+                }
+            </div>
 
         </div>
     );
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,12 +1,32 @@
 
+import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
-    const { name, flags } = country;
-    console.log(country);
+const Country = ({ country, handelvisitedcountry }) => {
+    const { name, flags, capital, population, area, ccn3 } = country;
+    // console.log(country);
+    //visited button conditioner  function 
+    const [visited, srtvisited] = useState(false)
+    const handelVisited = () => {
+        srtvisited(!visited)
+    }
+
     return (
-        <div className='Country_style'>
-            <img src={flags?.png} alt="" />
-            <h1>Name:{name?.common}</h1>
+        //add visited conditioner style
+        <div className={`Country_style ${visited && 'visited'}`}>
+            <img className='img_style' src={flags?.png} alt="" />
+            <h1>Name: {name?.common}</h1>
+            <h2>population :{population}</h2>
+            <h3>capital: {capital}</h3>
+            <h4>Area: {area}</h4>
+            <h5>Code: {ccn3}</h5>
+            <button onClick={() => handelvisitedcountry(country)}>Mark visited</button>
+            <br />
+            <div>
+                {/* visited button conditioner  chang  */}
+                <button onClick={handelVisited}>{visited ? 'visited' : 'Going'}</button>
+                {visited ? 'i have visited This Country' : 'i wont to visited'}
+                {/* visited button conditioner chang  */}
+            </div>
         </div>
     );
 };
